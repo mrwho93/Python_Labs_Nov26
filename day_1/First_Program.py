56ebc160988970be96cbeccdcb3b1bbcebcfea57
Winson Ho <winsonho93@gmail.com>
2024-11-26 16:46:20 +0000

    add day_1

---
+++ day_1/First_Program.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# Author: WHo
+# Version: 1.0
+# Description: This if the first script.
+
+#name = "Winson Ho"
+
+name=input("Enter name: ")
+
+print("My name is",name)
+
+import math
+print("Cosine of 0.5 is", math.cos(0.5))
+
+import random
+print("My lucky number is", random.randint(1,50))
+
+num = 42
+num
---
+++ day_1/ex2.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# Author: WHo
+# Version: 1.0
+# Description: Exercise 2 Q1
+
+first_name = "Winson"
+last_name = "Ho"
+
+print(first_name , last_name)
+
+list = [first_name, last_name]
+
+dict = {'first':first_name , 'last':last_name}
