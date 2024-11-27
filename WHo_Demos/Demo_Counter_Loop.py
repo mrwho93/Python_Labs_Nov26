8f808835a4a1e28b79427f640cc9347ac4f639e3
Winson Ho <winsonho93@gmail.com>
2024-11-27 10:15:52 +0000

    Added script to demo counter loops

---
+++ WHo_Demos/Demo_Counter_Loop.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# Author: WHo
+# Version: 1.0
+# Description: This script will demo how to repeat a block of 
+# commands a specific number of times using a counter loop.
+
+count = 0 # 1. Initialise counter
+while count < 10: #2. Test counter
+    print(count)
+    count += 1 #3. Increment counter
