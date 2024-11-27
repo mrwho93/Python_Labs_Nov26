b4e263bb2d604209d9b06eb07451fd4e1309c53c
Winson Ho <winsonho93@gmail.com>
2024-11-27 10:51:12 +0000

    Added script to simulate the bank PIN entry

---
+++ WHo_Demos/Demo_Pin.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# Author: WHo
+# Version: 1.0
+# Description: This script will simulate a high street bank PIN machine
+
+master_pin = "0123"
+pin = None
+attempts = 0
+
+while pin != master_pin and attempts < 3:
+    pin = input("Enter PIN: ")
+    if pin == master_pin:
+        print("Valid PIN")
+        break
+    else:
+        print("Invalid PIN")
+        attempts += 1
+else:
+    # EXECUTES only ONCE when while condition become false   
+    print("Too many attepts...")     
+    print("Your cards has been retained. Have aa nice day")
+print("Done")      
