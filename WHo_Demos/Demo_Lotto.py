7540abcaf1ac62e028361d7bca8961d4a5184608
Winson Ho <winsonho93@gmail.com>
2024-11-27 10:16:05 +0000

    No additional updates

---
+++ WHo_Demos/Demo_Lotto.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+# Author: WHo
+# Version: 1.0
+# Description: This script will generate 6 unique random numbers
+# numbers
+import random
+
+lotto = []
+
+while len(lotto) < 6:
+    num = random.randint(1, 50)
+    if num not in lotto:
+        lotto.append(num)
+    else:
+        print("Duplicate =", num)
+
+print("Lotter numbers =", lotto)        
+
+        
+
+
